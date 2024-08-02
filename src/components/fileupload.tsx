e0c247ef8c1f6e0887c8e8a55f6271e12cf0dafd
coreyabrown <coreyabrown.cb@gmail.com>
2024-08-02 12:08:04 -0500

    Add Snackbar Alerts

--- src/components/fileupload.tsx
+++ src/components/fileupload.tsx
@@ -1,12 +1,26 @@
 import { UploadFile, Download } from "@mui/icons-material";
 import { ChangeEvent, useState } from "react";
-import { Button } from "@mui/material";
+import { Button, Snackbar, SnackbarCloseReason, Alert, AlertProps } from "@mui/material";
 import { dataFile, fileValidation, downloadsPath } from "./datahandler";
 import fs from 'fs';
 import path from 'path';
 
 export function FileUpload() {
     const [filename, setFilename] = useState("");
+    const [sev, setSev] = useState("");
+    const [message, setMessage] = useState("");
+    const [open, setOpen] = useState(false);
+
+    const handleClose = (
+      event?: React.SyntheticEvent | Event,
+      reason?: SnackbarCloseReason,
+    ) => {
+      if (reason === 'clickaway') {
+        return;
+      }
+  
+      setOpen(false);
+    };
 
     const handleFileUpload = (e: ChangeEvent<HTMLInputElement>) => {
         if (!e.target.files) {
@@ -22,10 +36,20 @@ export function FileUpload() {
             if (fileValidation(fileContent)){
               fs.copyFileSync(file.path, dataFile)
               window.location.reload()
+              setSev('success');
+              setMessage('File successfully uploaded')
+              setOpen(true)
+            } else {
+              setSev('error');
+              setMessage('Incorrect file format')
+              setOpen(true)
             }
             
         } else {
           setFilename('')
+          setSev('error');
+          setMessage('An error occurred during the file upload process')
+          setOpen(true)
         }
     }
 
@@ -40,28 +64,64 @@ export function FileUpload() {
             Upload Data
             <input type="file" accept=".json" hidden onChange={handleFileUpload} />
           </Button>
+          <Snackbar
+            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+            open = {open}
+            autoHideDuration={5000}
+            onClose = {handleClose} >
+              <Alert
+                onClose = {handleClose}
+                severity= {sev as AlertProps["severity"]}
+                variant="filled"
+                > {message}
+              </Alert>
+          </Snackbar>
           </>
     );
 }
 
 export function FileDownload() {
+  const [open, setOpen] = useState(false);
 
   const dlFile = () => {
     fs.copyFileSync(dataFile, path.join(downloadsPath,'IowaTaxCredit_Export.json'));
-    alert('A file titled "IowaTaxCredit_Export.json" has been downloaded to your Downloads folder.');
+    setOpen(true);
   }
+
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: SnackbarCloseReason,
+  ) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   return (
     <>
       <Button
-            component="label"
-            variant="outlined"
-            onClick={() => dlFile()}
-            startIcon={<Download />}
-            sx={{ marginLeft: "1rem", float: "right" }}
-          >
-            Download Data
-          </Button>
-      </>
+        component="label"
+        variant="outlined"
+        onClick={() => dlFile()}
+        startIcon={<Download />}
+        sx={{ marginLeft: "1rem", float: "right" }}
+      >
+        Download Data
+      </Button>
+      <Snackbar
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        open = {open}
+        autoHideDuration={5000}
+        onClose = {handleClose} >
+          <Alert
+            onClose = {handleClose}
+            severity="success"
+            variant="filled"
+            >IowaTaxCredit_Export.json can be found in your Downloads folder
+          </Alert>
+      </Snackbar>
+    </>
 );
 }
